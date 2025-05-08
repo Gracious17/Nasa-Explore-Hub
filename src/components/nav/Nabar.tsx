dc456047974d37042eb8dc93a32b3f26396cef83
Gracious-kingsley <Kingsleygracious16@gmail.com>
2025-05-08 09:36:39 +0100

    made some changes

--- src/components/nav/Nabar.tsx
+++ src/components/nav/Nabar.tsx
@@ -1,12 +1,18 @@
 "use client"
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
-
+import { useState } from "react";
+import { AlignJustify, X } from "lucide-react";
 export function Navbar() {
+  const [navOpen,setNavOpen]=useState(false)
   const { data: session } = useSession()
+ 
+  const handleNavOpen=()=>{
+    setNavOpen(!navOpen)
+  }
   return (
     <nav className="bg-gray-900 p-4 flex justify-between items-center">
-      <div className="space-x-4">
+      <div className="space-x-4 hidden md:flex">
         <Link href="/">Home</Link>
         <Link href="/picture">Pictures</Link>
         <Link href="mars-weather">Mars</Link>
@@ -25,6 +31,21 @@ export function Navbar() {
           </button>
         )}
       </div>
+      <div className="relative md:hidden cursor-pointer border-2 border-white rounded-full p-2" onClick={handleNavOpen}>
+        {navOpen?(
+          <X size={25}/>
+        ):(  <AlignJustify size={25}/>)}
+      </div>
+      {navOpen && (
+        <div className=" absolute space-x-4 flex flex-col bg-gray-900 p-8 rounded shadow-md top-16 right-4 md:hidden">
+        <Link href="/">Home</Link>
+        <Link href="/picture">Pictures</Link>
+        <Link href="mars-weather">Mars</Link>
+        <Link href="/iss-tracker">ISS</Link>
+        <Link href="/asteroids">Asteroids</Link>
+        <Link href="/planets">Planets</Link>
+      </div>
+      )}
     </nav>
   );
 }
